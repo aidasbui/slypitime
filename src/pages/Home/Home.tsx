abd0df3a84a5ddcf9451eca09a347a444c683dd5
aidasbui <aidasbuivydas@gmail.com>
2024-04-28 21:56:10 +0300

    feat: add basic setup and start a home component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import Layout from '../../components/Layout';
 
 const Home = () => {
   return (
-    <div className="w-screen h-screen bg-red-300 text-3xl ">
-      Hi Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo, sed
-      reprehenderit fugit earum assumenda culpa repudiandae itaque dolores dolore! Velit
-      numquam repudiandae tempora odit autem fuga cupiditate placeat quasi.
-    </div>
+    <Layout>
+      <div className="flex flex-col w-full gap-12 items-center">
+        <h1 className="text-4xl font-bold text-center">Slypitime</h1>
+
+        <p className="text-left whitespace-pre-line">
+          {`Welcome to Sleepytime! This is a simple app that helps you quickly calculate an estimate of when to set your alarm. 
+          
+          Click on the button below to see the times
+          you should try waking up at. 
+          
+          Please note that these are only estimates and may
+          not work for everyone and additional 20 minutes are added to give you time to
+          fall asleep.`}
+        </p>
+
+        <div className="w-full flex justify-center items-center"></div>
+      </div>
+    </Layout>
   );
 };
 
